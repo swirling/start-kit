c85bf2a4ea6356406a50d18247a24797b3f1820f
yang.li <yang.li@feitian-tech.com>
2017-07-04 14:19:05 +0800

    update: add withState

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { compose, withProps, lifecycle } from 'recompose';
+import { compose, withProps, lifecycle, withState } from 'recompose';
 import { addCount, loadCount } from '../../actions/counter';
 import './index.less';
 
 export const Counter = props =>
   (<div className="a">
     <div className="b">{props.count}----{props.a}</div>
+    <input onInput={props.setA} value={props.a} />
     <button onClick={() => props.addCount(1)}>addone</button>
   </div>);
 
@@ -15,6 +16,7 @@ Counter.propTypes = {
   count: PropTypes.number.isRequired,
   a: PropTypes.string.isRequired,
   addCount: PropTypes.func.isRequired,
+  setA: PropTypes.func.isRequired,
 };
 
 export default compose(
@@ -25,6 +27,7 @@ export default compose(
     { addCount, loadCount },
   ),
   withProps({ a: 1 }),
+  withState('a', 'setA', null),
   lifecycle({
     componentDidMount() {
       this.props.loadCount();
